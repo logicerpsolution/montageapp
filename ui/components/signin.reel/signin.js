41887db8fa16b88917c32395b5ae29876dac3c92
logicerpsolution <noreply>
2015-12-30 06:39:05 +0000

    Update component ui/components/signin.reel/signin.js

--- ui/components/signin.reel/signin.js
+++ ui/components/signin.reel/signin.js
@@ -4,4 +4,16 @@
 var Montage = require("montage/core/core").Montage,
     Component = require("montage/ui/component").Component;
 
+/**
+ * @class Signin
+ * @extends Component
+ */
+//exports.Signin = Component.specialize(/** @lends Signin# */ {
+   // constructor: {
+    //    value: function Signin() {
+//            this.super();
+     //   }
+  //  }
+//});
+
 exports.Signin=Montage.create(Component);
