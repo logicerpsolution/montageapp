691ebdbab3636ebc2ad97d926bc2d7f863a2eefc
logicerpsolution <noreply>
2015-12-30 06:22:43 +0000

    Add component sidebar

---
+++ ui/sidebar.reel/sidebar.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/sidebar.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Sidebar
+ * @extends Component
+ */
+exports.Sidebar = Component.specialize(/** @lends Sidebar# */ {
+    constructor: {
+        value: function Sidebar() {
+            this.super();
+        }
+    }
+});
