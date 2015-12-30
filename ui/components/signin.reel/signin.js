ddcb9eca33e2b9983cf07bdcdf8a41f4cf87873e
logicerpsolution <noreply>
2015-12-30 06:38:07 +0000

    Update component ui/components/signin.reel/signin.js

--- ui/components/signin.reel/signin.js
+++ ui/components/signin.reel/signin.js
@@ -1,7 +1,8 @@
 /**
  * @module ui/components/signin.reel
  */
-var Component = require("montage/ui/component").Component;
+var Montage = require("montage/core/core").Montage,
+    Component = require("montage/ui/component").Component;
 
 /**
  * @class Signin
