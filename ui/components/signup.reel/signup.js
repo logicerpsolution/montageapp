8d65671f37cbbb0820b36fdc0f86edc864fe5356
logicerpsolution <noreply>
2015-12-30 06:39:38 +0000

    Add component signup

---
+++ ui/components/signup.reel/signup.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/components/signup.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Signup
+ * @extends Component
+ */
+exports.Signup = Component.specialize(/** @lends Signup# */ {
+    constructor: {
+        value: function Signup() {
+            this.super();
+        }
+    }
+});
