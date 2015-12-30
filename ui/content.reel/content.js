652c2362f55e5370116175f10df593c7d0c65bd1
logicerpsolution <noreply>
2015-12-30 06:34:06 +0000

    Update component ui/content.reel/content.js

--- ui/content.reel/content.js
+++ ui/content.reel/content.js
@@ -8,10 +8,38 @@ var Montage = require("montage/core/core").Montage,
  * @class Content
  * @extends Component
  */
-exports.Content = Component.specialize(/** @lends Content# */ {
-    constructor: {
-        value: function Content() {
-            this.super();
+exports.Content = Montage.create(Component, {
+    // the main component
+    sandbox: {
+        value: null
+    },
+
+    contentDeck: {
+        value: null
+    },
+
+    _selectedItem: {value: null},
+    selectedItem: {
+        get: function() {
+            return this._selectedItem;
+        },
+        set: function(value) {
+            this._selectedItem = value;
+            this.needsDraw = true;
+        }
+    },
+
+    slotDidSwitchContent: {
+        value: function(substitution, nodeShown, componentShown, nodeHidden, componentHidden) {
+            console.log('substitution did switch content');
+
+            if(componentHidden && typeof componentHidden.didBecomeInactiveInSlot === 'function') {
+                componentHidden.didBecomeInactiveInSlot();
+            }
+            if(componentShown && typeof componentShown.didBecomeActiveInSlot === 'function') {
+                componentShown.didBecomeActiveInSlot();
+            }
         }
     }
+
 });
