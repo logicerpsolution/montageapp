be0260d77f269f14ca7b1cce9cf2b24705df27c6
logicerpsolution <noreply>
2015-12-30 06:41:25 +0000

    Update component ui/components/signup.reel/signup.js

--- ui/components/signup.reel/signup.js
+++ ui/components/signup.reel/signup.js
@@ -1,7 +1,8 @@
 /**
  * @module ui/components/signup.reel
  */
-var Component = require("montage/ui/component").Component;
+var Montage = require("montage/core/core").Montage,
+    Component = require("montage/ui/component").Component;
 
 /**
  * @class Signup
