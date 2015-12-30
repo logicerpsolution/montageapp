fbd4c239036d129d828f30514c094c7994bba103
logicerpsolution <noreply>
2015-12-30 06:24:43 +0000

    Update component ui/sidebar.reel/sidebar.js

--- ui/sidebar.reel/sidebar.js
+++ ui/sidebar.reel/sidebar.js
@@ -1,7 +1,8 @@
 /**
  * @module ui/sidebar.reel
  */
-var Component = require("montage/ui/component").Component;
+var Montage = require("montage/core/core").Montage,
+    Component = require("montage/ui/component").Component;
 
 /**
  * @class Sidebar
