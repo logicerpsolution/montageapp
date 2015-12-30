ec73b37f3d4379ae7c86cf5f7d9294405092086e
logicerpsolution <noreply>
2015-12-30 06:26:24 +0000

    Add component content

---
+++ ui/content.reel/content.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/content.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Content
+ * @extends Component
+ */
+exports.Content = Component.specialize(/** @lends Content# */ {
+    constructor: {
+        value: function Content() {
+            this.super();
+        }
+    }
+});
